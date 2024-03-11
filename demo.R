9656d015a4789097147c185a2e2a1651966e5512
Takaaki Masaki <takaakimasaki@users.noreply.github.com>
2024-03-11 13:48:26 +0100

    added refugee data

---
+++ demo.R
@@ -0,0 +1,8 @@
+#set up
+install.packages("pacman")
+pacman::p_load(dplyr, tidyverse, WDI, ggplot2, ggthemes, here)
+
+#task 1: loading WDI data on refugees and save it as csv
+WDI::WDIsearch('refugee*')
+dat = WDI::WDI(indicator=c('SM.POP.REFG'), start=1960, end=2021)
+dat %>% write_csv(here::here("","data","refugee_stats.csv"))
